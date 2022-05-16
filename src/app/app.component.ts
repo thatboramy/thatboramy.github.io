a0ab78a88fd1cfa65470225533b5fda93825c2e2
thatboramy <mcboramy@gmail.com>
2022-05-15 17:10:39 -0700

    major styling overhaul

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -115,6 +115,15 @@ export class AppComponent {
   saturdayHashMap: any = {};
   sundayHashMap: any = {};
 
+  tabsInfo = [
+    { name: 'Friday', map: this.fridayHashMap },
+    {
+      name: 'Saturday',
+      map: this.saturdayHashMap,
+    },
+    { name: 'Sunday', map: this.sundayHashMap },
+  ];
+
   venueLocationGroup: any[] = [];
   venueLocationGroupNames: any[] = [];
   constructor() {
@@ -134,16 +143,19 @@ export class AppComponent {
       b = this.convertTime12to24(b);
       return a > b ? 1 : a < b ? -1 : 0;
     });
+
     // because time sort sucks
     this.timeValues = [
       '8:55 am',
       '9:00 am',
+      '9:20 am',
       '9:30 am',
       '10:00 am',
       '10:30 am',
       '10:45 am',
       '11:00 am',
       '11:30 am',
+      '11:45 am',
       '12:00 pm',
       '12:15 pm',
       '12:30 pm',
@@ -152,30 +164,37 @@ export class AppComponent {
       '1:30 pm',
       '1:45 pm',
       '2:00 pm',
+      '2:15 pm',
+      '2:30 pm',
+      '2:45 pm',
       '3:00 pm',
+      '3:45 pm',
       '4:00 pm',
+      '4:15 pm',
       '4:30 pm',
       '4:45 pm',
       '5:00 pm',
       '5:30 pm',
+      '5:45 pm',
       '6:00 pm',
+      '6:30 pm',
       '7:00 pm',
       '7:30 pm',
       '8:00 pm',
       '8:30 pm',
       '9:00 pm',
+      '9:30 pm',
       '10:00 pm',
       '10:30 pm',
+      '11:00 pm',
+      '11:30 pm',
+      '11:55 pm',
+      '1:00 am',
+      '2:00 am',
     ];
   }
   groupVenues() {
     this.venueLocationGroupNames = [
-      {
-        name: 'Exhibitor Floor (Ground Floor)',
-        subVenues: [
-          { subVenueTitle: 'Celebrity Halls', rooms: this.HallLocation },
-        ],
-      },
       {
         name: 'North Building',
         subVenues: [
@@ -206,6 +225,12 @@ export class AppComponent {
             subVenueTitle: 'Special Building',
             rooms: this.WestBuildingSpecialLocation,
           },
+        ],
+      },
+      {
+        name: 'Exhibitor Floor (Ground Floor)',
+        subVenues: [
+          { subVenueTitle: 'Celebrity Halls', rooms: this.HallLocation },
         ],
       },
       {
@@ -267,6 +292,12 @@ export class AppComponent {
           !this.timeValues.some((loc) => loc === tempEventStartTime)
         ) {
           this.timeValues.push(tempEventStartTime);
+        }
+        if (
+          tempEventEndTime !== '' &&
+          !this.timeValues.some((loc) => loc === tempEventEndTime)
+        ) {
+          this.timeValues.push(tempEventEndTime);
         }
         const tempEvent = {
           name: tempEventName,
--- src/app/table-event-schedule/table-event-schedule.component.ts
+++ src/app/table-event-schedule/table-event-schedule.component.ts
@@ -10,9 +10,11 @@ export class TableEventScheduleComponent implements OnInit {
   @Input() venues: string[] = [];
   @Input() eventMap = {};
   @Input() isMobile = false;
+  @Input() showTimeBlock = true;
 
   public venueLocationsDisplay: string[] = [];
   public timeValuesDisplay: string[] = [];
+  public timeStartValues: string[] = [];
   public eventHashMap: any = {};
 
   constructor() {}
@@ -23,9 +25,24 @@ export class TableEventScheduleComponent implements OnInit {
       return this.hasEventAtLocation(room);
     });
     this.venueLocationsDisplay = ['time', ...availableVenueLocations];
-    this.timeValuesDisplay = this.timeValues.filter((time) => {
-      return this.hasEventAtTime(time);
+
+    this.timeStartValues = this.timeValues.filter((time) => {
+      return this.hasEventAtStartTime(time);
     });
+
+    if (this.showTimeBlock) {
+      this.timeValuesDisplay = this.timeValues.filter((time) => {
+        return this.hasEventAtEndTime(time) || this.hasEventAtStartTime(time);
+      });
+      console.log(this.eventHashMap);
+      this.hasEventAtEndTime('5:30 pm');
+      console.log(this.timeValues);
+      console.log(this.timeValuesDisplay);
+    } else {
+      this.timeValuesDisplay = this.timeValues.filter((time) => {
+        return this.hasEventAtStartTime(time);
+      });
+    }
   }
 
   hasEventAtLocation(roomQuery: string) {
@@ -36,13 +53,31 @@ export class TableEventScheduleComponent implements OnInit {
     }
   }
 
-  hasEventAtTime(timeQuery: string) {
+  hasEventAtStartTime(timeQuery: string) {
     let hasEvent = false;
     this.venues.forEach((room) => {
       if (this.eventHashMap[room]) {
         if (this.eventHashMap[room][timeQuery]) {
           hasEvent = true;
         }
+      }
+    });
+    return hasEvent;
+  }
+  //5:30pm
+  hasEventAtEndTime(timeQuery: string) {
+    let hasEvent = false;
+    this.venues.forEach((room) => {
+      if (this.eventHashMap[room]) {
+        this.timeValues.forEach((possibleStartTime) => {
+          if (this.eventHashMap[room][possibleStartTime]) {
+            const endTime = this.eventHashMap[room][possibleStartTime].endTime;
+            if (timeQuery === endTime) {
+              hasEvent = true;
+              return;
+            }
+          }
+        });
       }
     });
     return hasEvent;
@@ -61,5 +96,58 @@ export class TableEventScheduleComponent implements OnInit {
     } else {
       return `${event.startTime}-${event.endTime}`;
     }
+  }
+
+  isStartTime(timeQuery: string) {
+    return this.timeStartValues.includes(timeQuery);
+  }
+
+  somethingHappening(room: any, time: any) {
+    let somethingHappens = false;
+    if (this.showTimeBlock) {
+      // if it's a start time
+      // if (this.timeStartValues.includes(time)) {
+      //   if (this.eventHashMap[room][time]) {
+      //     somethingHappens = true;
+      //   }
+      // } else {
+      //   // if it's end time
+      //   if (this.eventHashMap[room]) {
+      //     this.timeValues.forEach((possibleStartTime) => {
+      //       if (this.eventHashMap[room][possibleStartTime]) {
+      //         const endTime =
+      //           this.eventHashMap[room][possibleStartTime].endTime;
+      //         if (time === endTime) {
+      //           somethingHappens = true;
+      //           return;
+      //         }
+      //       }
+      //     });
+      //   }
+      // }
+      // is it  middle child
+      if (somethingHappens === false) {
+        if (this.eventHashMap[room]) {
+          this.timeValues.forEach((possibleStartTime) => {
+            if (this.eventHashMap[room][possibleStartTime]) {
+              const endTime =
+                this.eventHashMap[room][possibleStartTime].endTime;
+              const startTime = possibleStartTime;
+
+              const startTimeIndex = this.timeValues.indexOf(startTime);
+              const endTimeIndex = this.timeValues.indexOf(endTime);
+              const timeQueryIndex = this.timeValues.indexOf(time);
+
+              if (startTimeIndex <= timeQueryIndex) {
+                if (timeQueryIndex < endTimeIndex) {
+                  somethingHappens = true;
+                }
+              }
+            }
+          });
+        }
+      }
+    }
+    return somethingHappens;
   }
 }
